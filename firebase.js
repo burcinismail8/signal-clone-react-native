92000cd07f038d43f9b5103ab4c3c017d86d367b
Burchin Ismail <burcinismail8@gmail.com>
2022-11-25 00:41:03 +0200

    Setup react-native-nav and firebase

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCiZUoJ08GNiwHfuSHupJJoX9v6rZ6KssU",
+  authDomain: "signal-clone-ba6cb.firebaseapp.com",
+  projectId: "signal-clone-ba6cb",
+  storageBucket: "signal-clone-ba6cb.appspot.com",
+  messagingSenderId: "287714823586",
+  appId: "1:287714823586:web:ee7d14c291b2c3e2c6b6fb",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
